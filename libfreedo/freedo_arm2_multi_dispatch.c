dec0fd0f9a5f88dfa866386fef4b4d8c15492332
Antonio SJ Musumeci <trapexit@spawn.link>
2018-06-02 21:39:52 -0400

    checkpoint

--- libfreedo/freedo_arm2_multi_dispatch.c
+++ libfreedo/freedo_arm2_multi_dispatch.c
@@ -598,54 +598,8 @@ IMM_AND_EOR_SUB_RSB_ADD_ADC_SDC_RSC(const uint32_t opcode_)
 static
 INLINE
 uint32_t
-SHIFT_DAC(const uint32_t s_,
-          const uint32_t val_)
-{
-  switch(s_ & 0x7)
-    {
-      /* IMM, logical left */
-    case 0x0:
-      return (val_ << (s_ >> 3));
-      /* REG, logical left */
-    case 0x1:
-      return (val_ << (CPU.REGS->R[s_ >> 4] & 0xFF));
-      /* IMM, logical right */
-    case 0x2:
-      return (val_ >> (s_ >> 3));
-      /* REG, logical right */
-    case 0x3:
-      return (val_ >> (CPU.REGS->R[s_ >> 4] & 0xFF));
-      /* IMM, arithmetic right */
-      /* WARNING: signed right shifts are often but not always
-         arithmetic. It might be required to use a more thorough
-         computation. */
-    case 0x4:
-      {
-        const uint32_t s = (s_ >> 3);
-
-        return ((int32_t)val_ >> ((s == 0) ? 32 : s));
-      }
-      /* REG, arithmetic right */
-    case 0x5:
-      {
-        const uint32_t s = (CPU.REGS->R[s_ >> 4] & 0xFF);
-
-        return ((int32_t)val_ >> ((s == 0) ? 32 : s));
-      }
-      /* IMM, rotate right */
-    case 0x6:
-      return ROR(val_,(s_ >> 3));
-      /* REG, rotate right */
-    case 0x7:
-      return ROR(val_,(CPU.REGS->R[s_ >> 4] & 0xFF));
-    }
-}
-
-static
-INLINE
-uint32_t
-SHIFT_SCC(const uint32_t s_,
-          const uint32_t val_)
+SHIFT(const uint32_t s_,
+      const uint32_t val_)
 {
   switch(s_ & 0x7)
     {
@@ -698,7 +652,7 @@ REG_AND_EOR_SUB_RSB_ADD_ADC_SDC_RSC(const uint32_t opcode_)
   const uint32_t shift  = ((opcode_ & 0x00000FF0) >>  4);
   const uint32_t Rm     = ((opcode_ & 0x0000000F) >>  0);
   const uint32_t op1    = CPU.REGS->R[Rn];
-  const uint32_t op2    = SHIFT_DAC(shift,Rm);
+  const uint32_t op2    = SHIFT(shift,Rm);
 
   switch(opcode)
     {
