d8d6f40453d9356dcc66d39b2b9924aaa354c98f
Antonio SJ Musumeci <trapexit@spawn.link>
2026-06-11 19:00:17 -0500

    DSPP: fast path submixer8x2 56

--- libopera/opera_dsp.c
+++ libopera/opera_dsp.c
@@ -129,6 +129,7 @@
 #define DSP_SQUARE_LFO_16_WORDS  14
 #define DSP_SUBMIXER2X2_20_WORDS 18
 #define DSP_SUBMIXER4X2_32_WORDS 30
+#define DSP_SUBMIXER8X2_56_WORDS 54
 #define DSP_SUBTRACT_INSN        0x6647
 
 #pragma pack(push,1)
@@ -696,6 +697,11 @@ static bool     dsp_fast_submixer2x2_20(uint32_t        *Y_,
                                         uint32_t        *RBSR_,
                                         bool            *work_);
 static bool     dsp_fast_submixer4x2_32(uint32_t        *Y_,
+                                        dsp_alu_flags_t *flags_,
+                                        int             *fExact_,
+                                        uint32_t        *RBSR_,
+                                        bool            *work_);
+static bool     dsp_fast_submixer8x2_56(uint32_t        *Y_,
                                         dsp_alu_flags_t *flags_,
                                         int             *fExact_,
                                         uint32_t        *RBSR_,
@@ -2880,6 +2886,53 @@ bool
 dsp_fast_submixer4x2_32_match(uint32_t pc_)
 {
   return dsp_fast_submixer4x2_32_base_match(pc_);
+}
+
+static
+bool
+dsp_fast_submixer8x2_56_base_match(uint32_t const pc_)
+{
+  static uint32_t const vals[DSP_SUBMIXER8X2_56_WORDS] = {
+    0x00005C80,0x00008000,0x0000801D,0x00005C27,
+    0x00008000,0x00008020,0x00005C27,0x00008000,
+    0x00008023,0x00005C27,0x00008000,0x00008026,
+    0x00005C27,0x00008000,0x00008029,0x00005C27,
+    0x00008000,0x0000802C,0x00005C27,0x00008000,
+    0x0000802F,0x00005C27,0x00008000,0x00008032,
+    0x00008000,0x00002080,0x00008000,0x00005C80,
+    0x00008000,0x00008000,0x00005C27,0x00008000,
+    0x00008000,0x00005C27,0x00008000,0x00008000,
+    0x00005C27,0x00008000,0x00008000,0x00005C27,
+    0x00008000,0x00008000,0x00005C27,0x00008000,
+    0x00008000,0x00005C27,0x00008000,0x00008000,
+    0x00005C27,0x00008000,0x00008000,0x00008000,
+    0x00002080,0x00008000
+  };
+  static uint32_t const masks[DSP_SUBMIXER8X2_56_WORDS] = {
+    0x0000FFFF,0x0002FC00,0x0002FC00,0x0000FFFF,
+    0x0002FC00,0x0002FC00,0x0000FFFF,0x0002FC00,
+    0x0002FC00,0x0000FFFF,0x0002FC00,0x0002FC00,
+    0x0000FFFF,0x0002FC00,0x0002FC00,0x0000FFFF,
+    0x0002FC00,0x0002FC00,0x0000FFFF,0x0002FC00,
+    0x0002FC00,0x0000FFFF,0x0002FC00,0x0002FC00,
+    0x0000FFFF,0x0000FFFF,0x0002FC00,0x0000FFFF,
+    0x0002FC00,0x0000FC00,0x0000FFFF,0x0002FC00,
+    0x0000FC00,0x0000FFFF,0x0002FC00,0x0000FC00,
+    0x0000FFFF,0x0002FC00,0x0000FC00,0x0000FFFF,
+    0x0002FC00,0x0000FC00,0x0000FFFF,0x0002FC00,
+    0x0000FC00,0x0000FFFF,0x0002FC00,0x0000FC00,
+    0x0000FFFF,0x0002FC00,0x0000FC00,0x0000FFFF,
+    0x0000FFFF,0x0002FC00
+  };
+
+  return dsp_fast_pattern_match(pc_,DSP_SUBMIXER8X2_56_WORDS,vals,masks);
+}
+
+static
+bool
+dsp_fast_submixer8x2_56_match(uint32_t pc_)
+{
+  return dsp_fast_submixer8x2_56_base_match(pc_);
 }
 
 static
@@ -5448,6 +5501,8 @@ dsp_fast_rebuild(void)
           DSP_FAST_TABLE[pc] = dsp_fast_submixer2x2_20;
         else if(dsp_fast_submixer4x2_32_match(pc))
           DSP_FAST_TABLE[pc] = dsp_fast_submixer4x2_32;
+        else if(dsp_fast_submixer8x2_56_match(pc))
+          DSP_FAST_TABLE[pc] = dsp_fast_submixer8x2_56;
         else if(dsp_fast_benchmark_6_match(pc))
           DSP_FAST_TABLE[pc] = dsp_fast_benchmark_6;
         else if(dsp_fast_dcsqxdhalfmono_59_match(pc))
@@ -6409,6 +6464,27 @@ dsp_fast_submixer4x2_32(uint32_t        *Y_,
     return false;
 
   return dsp_fast_interpret_block(pc,pc + DSP_SUBMIXER4X2_32_WORDS,
+                                  Y_,flags_,fExact_,RBSR_,work_);
+}
+
+static
+bool
+dsp_fast_submixer8x2_56(uint32_t        *Y_,
+                        dsp_alu_flags_t *flags_,
+                        int             *fExact_,
+                        uint32_t        *RBSR_,
+                        bool            *work_)
+{
+  uint32_t pc;
+
+  if(DSP.flags.nOP_MASK != 0xFFFF)
+    return false;
+
+  pc = DSP.dregs.PC;
+  if(!dsp_fast_submixer8x2_56_base_match(pc))
+    return false;
+
+  return dsp_fast_interpret_block(pc,pc + DSP_SUBMIXER8X2_56_WORDS,
                                   Y_,flags_,fExact_,RBSR_,work_);
 }
 
