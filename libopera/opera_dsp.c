f98e6005cb7f0ab82b81e4724991df2989476d1a
Antonio SJ Musumeci <trapexit@spawn.link>
2026-06-12 17:12:24 -0500

    DSPP: implement fixedstereosample 16 fast paths

--- libopera/opera_dsp.c
+++ libopera/opera_dsp.c
@@ -13986,6 +13986,142 @@ dsp_fast_fixedstereo8_19(uint32_t        *Y_,
 }
 
 static
+void
+dsp_fast_fixedstereosample_16_run(uint32_t const  base,
+                                  uint32_t       *Y_,
+                                  dsp_alu_flags_t *flags_,
+                                  int             *fExact_)
+{
+  uint32_t a;
+  uint32_t b;
+  ITAG_t branch;
+  ITAG_t dst;
+  ITAG_t imm;
+  ITAG_t inst;
+  ITAG_t src1;
+  ITAG_t src2;
+  uint32_t y;
+
+  inst.raw = DSP.NMem[base + 0];
+  src1.raw = DSP.NMem[base + 1];
+  imm.raw  = DSP.NMem[base + 2];
+
+  DSP.flags.req.raw   = DSP.INSTTRAS[inst.raw].req.raw;
+  DSP.flags.BS        = DSP.INSTTRAS[inst.raw].BS;
+  DSP.flags.WRITEBACK = 0;
+
+  DSP.dregs.PC = base + 2;
+  DSP.flags.WRITEBACK = src1.nrof.OP_ADDR;
+  DSP.flags.ALU1 = dsp_read(DSP.flags.WRITEBACK);
+
+  DSP.dregs.PC = base + 3;
+  DSP.flags.ALU2 = (uint16_t)(imm.iof.IMMEDIATE << (imm.iof.JUSTIFY & 3));
+  DSP.flags.WRITEBACK = 0;
+
+  a = ((uint32_t)(uint16_t)DSP.flags.ALU1 << 16);
+  b = ((uint32_t)(uint16_t)DSP.flags.ALU2 << 16);
+  if(inst.aif.ALU == 2)
+    {
+      y = (a + b);
+      flags_->carry    = ADD_CFLAG(a,b,y);
+      flags_->overflow = ADD_VFLAG(a,b,y);
+    }
+  else
+    {
+      y = (a & b);
+      flags_->carry    = 0;
+      flags_->overflow = 0;
+    }
+
+  flags_->zero     = ((y & 0xFFFF0000) ? 0 : 1);
+  flags_->negative = ((y >> 31) ? 1 : 0);
+  *fExact_         = ((y & 0x0000F000) ? 0 : 1);
+  *Y_              = y;
+
+  imm.raw = DSP.NMem[base + 4];
+
+  DSP.flags.req.raw   = DSP.INSTTRAS[DSP.NMem[base + 3]].req.raw;
+  DSP.flags.BS        = DSP.INSTTRAS[DSP.NMem[base + 3]].BS;
+  DSP.flags.WRITEBACK = 0;
+
+  DSP.dregs.PC = base + 5;
+  DSP.flags.ALU1 = (uint16_t)(imm.iof.IMMEDIATE << (imm.iof.JUSTIFY & 3));
+
+  a = *Y_;
+  b = ((uint32_t)(uint16_t)DSP.flags.ALU1 << 16);
+  y = (a - b);
+
+  flags_->carry    = SUB_CFLAG(a,b,y);
+  flags_->overflow = SUB_VFLAG(a,b,y);
+  flags_->zero     = ((y & 0xFFFF0000) ? 0 : 1);
+  flags_->negative = ((y >> 31) ? 1 : 0);
+  *fExact_         = ((y & 0x0000F000) ? 0 : 1);
+  *Y_              = y;
+
+  branch.raw = DSP.NMem[base + 5];
+  DSP.dregs.PC = base + 6;
+  if(1 & DSP.BRCONDTAB[branch.br.bits][*fExact_ + ((flags_->raw * 0x10080402) >> 24)])
+    {
+      DSP.dregs.PC = branch.cif.BCH_ADDR;
+      return;
+    }
+
+  src1.raw = DSP.NMem[base + 7];
+  src2.raw = DSP.NMem[base + 8];
+  dst.raw  = DSP.NMem[base + 9];
+
+  DSP.flags.req.raw   = DSP.INSTTRAS[DSP.NMem[base + 6]].req.raw;
+  DSP.flags.BS        = DSP.INSTTRAS[DSP.NMem[base + 6]].BS;
+  DSP.flags.WRITEBACK = 0;
+
+  DSP.dregs.PC = base + 8;
+  DSP.flags.WRITEBACK = src1.nrof.OP_ADDR;
+  DSP.flags.MULT1 = dsp_read(DSP.flags.WRITEBACK);
+
+  DSP.dregs.PC = base + 9;
+  DSP.flags.WRITEBACK = src2.nrof.OP_ADDR;
+  DSP.flags.MULT2 = dsp_read(DSP.flags.WRITEBACK);
+
+  DSP.dregs.PC = base + 10;
+  DSP.flags.WRITEBACK = dst.nrof.OP_ADDR;
+  (void)dsp_read(DSP.flags.WRITEBACK);
+
+  y = dsp_fast_multiply_product_value(DSP.flags.MULT1,DSP.flags.MULT2);
+  dsp_fast_set_product_flags(y,flags_,fExact_);
+
+  *Y_ = y;
+  if(DSP.flags.WRITEBACK)
+    dsp_write(DSP.flags.WRITEBACK,((int32_t)y) >> 16);
+
+  src1.raw = DSP.NMem[base + 11];
+  src2.raw = DSP.NMem[base + 12];
+  dst.raw  = DSP.NMem[base + 13];
+
+  DSP.flags.req.raw   = DSP.INSTTRAS[DSP.NMem[base + 10]].req.raw;
+  DSP.flags.BS        = DSP.INSTTRAS[DSP.NMem[base + 10]].BS;
+  DSP.flags.WRITEBACK = 0;
+
+  DSP.dregs.PC = base + 12;
+  DSP.flags.WRITEBACK = src1.nrof.OP_ADDR;
+  DSP.flags.MULT1 = dsp_read(DSP.flags.WRITEBACK);
+
+  DSP.dregs.PC = base + 13;
+  DSP.flags.WRITEBACK = src2.nrof.OP_ADDR;
+  DSP.flags.MULT2 = dsp_read(DSP.flags.WRITEBACK);
+
+  DSP.dregs.PC = base + 14;
+  DSP.flags.WRITEBACK = dst.nrof.OP_ADDR;
+  (void)dsp_read(DSP.flags.WRITEBACK);
+
+  y = dsp_fast_multiply_product_value(DSP.flags.MULT1,DSP.flags.MULT2);
+  dsp_fast_set_product_flags(y,flags_,fExact_);
+
+  *Y_ = y;
+  if(DSP.flags.WRITEBACK)
+    dsp_write(DSP.flags.WRITEBACK,((int32_t)y) >> 16);
+}
+
+static
 bool
 dsp_fast_fixedstereosample_16_1ic(uint32_t        *Y_,
                                   dsp_alu_flags_t *flags_,
@@ -14001,9 +14137,9 @@ dsp_fast_fixedstereosample_16_1ic(uint32_t        *Y_,
   if(!dsp_fast_fixedstereosample_16_1ic_base_match(base))
     return false;
 
-  return dsp_fast_interpret_block(base,
-                                  base + DSP_FIXEDSTEREOSAMPLE_16_1IC_WORDS,
-                                  Y_,flags_,fExact_,RBSR_,work_);
+  dsp_fast_fixedstereosample_16_run(base,Y_,flags_,fExact_);
+
+  return true;
 }
 
 static
@@ -14022,9 +14158,9 @@ dsp_fast_fixedstereosample_16_270(uint32_t        *Y_,
   if(!dsp_fast_fixedstereosample_16_270_base_match(base))
     return false;
 
-  return dsp_fast_interpret_block(base,
-                                  base + DSP_FIXEDSTEREOSAMPLE_16_270_WORDS,
-                                  Y_,flags_,fExact_,RBSR_,work_);
+  dsp_fast_fixedstereosample_16_run(base,Y_,flags_,fExact_);
+
+  return true;
 }
 
 static
