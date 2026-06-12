2d6f69f2d1770a9bbcee72800388b7964d4c8560
Antonio SJ Musumeci <trapexit@spawn.link>
2026-06-12 02:10:18 -0500

    DSPP: implement splitexec 7 fast path

--- libopera/opera_dsp.c
+++ libopera/opera_dsp.c
@@ -8027,7 +8027,12 @@ dsp_fast_splitexec_7(uint32_t        *Y_,
                      uint32_t        *RBSR_,
                      bool            *work_)
 {
+  uint32_t a;
+  uint32_t b;
+  ITAG_t branch;
   uint32_t pc;
+  ITAG_t src;
+  uint32_t y;
 
   if(DSP.flags.nOP_MASK != 0xFFFF)
     return false;
@@ -8036,8 +8041,52 @@ dsp_fast_splitexec_7(uint32_t        *Y_,
   if(!dsp_fast_splitexec_7_base_match(pc))
     return false;
 
-  return dsp_fast_interpret_block(pc,pc + DSP_SPLITEXEC_7_WORDS,
-                                  Y_,flags_,fExact_,RBSR_,work_);
+  branch.raw = DSP.NMem[pc + 4];
+  if(branch.cif.BCH_ADDR != ((pc + DSP_SPLITEXEC_7_WORDS) & 0x3FF))
+    return false;
+
+  src.raw = DSP.NMem[pc + 2];
+
+  DSP.flags.req.raw   = DSP.INSTTRAS[DSP.NMem[pc + 0]].req.raw;
+  DSP.flags.BS        = DSP.INSTTRAS[DSP.NMem[pc + 0]].BS;
+  DSP.flags.WRITEBACK = 0;
+
+  DSP.dregs.PC = pc + 2;
+  DSP.flags.WRITEBACK = 0x105;
+  DSP.flags.ALU1 = dsp_read(DSP.flags.WRITEBACK);
+
+  DSP.dregs.PC = pc + 3;
+  DSP.flags.WRITEBACK = src.nrof.OP_ADDR;
+  DSP.flags.ALU2 = dsp_read(DSP.flags.WRITEBACK);
+  DSP.flags.WRITEBACK = 0;
+
+  a = ((uint32_t)(uint16_t)DSP.flags.ALU1 << 16);
+  b = ((uint32_t)(uint16_t)DSP.flags.ALU2 << 16);
+  y = (a & b);
+
+  flags_->carry    = 0;
+  flags_->overflow = 0;
+  flags_->zero     = ((y & 0xFFFF0000) ? 0 : 1);
+  flags_->negative = ((y >> 31) ? 1 : 0);
+  *fExact_         = ((y & 0x0000F000) ? 0 : 1);
+  *Y_              = y;
+
+  DSP.dregs.PC = pc + 4;
+  DSP.flags.req.raw   = DSP.INSTTRAS[DSP.NMem[pc + 3]].req.raw;
+  DSP.flags.BS        = DSP.INSTTRAS[DSP.NMem[pc + 3]].BS;
+  DSP.flags.WRITEBACK = 0;
+
+  flags_->carry    = 0;
+  flags_->overflow = 0;
+  flags_->zero     = ((*Y_ & 0xFFFF0000) ? 0 : 1);
+  flags_->negative = ((*Y_ >> 31) ? 1 : 0);
+  *fExact_         = ((*Y_ & 0x0000F000) ? 0 : 1);
+
+  DSP.dregs.PC = pc + DSP_SPLITEXEC_7_WORDS;
+  if(1 & DSP.BRCONDTAB[branch.br.bits][*fExact_ + ((flags_->raw * 0x10080402) >> 24)])
+    DSP.dregs.PC = branch.cif.BCH_ADDR;
+
+  return true;
 }
 
 static
